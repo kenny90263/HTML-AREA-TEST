306d54f87bd6f05f025cccd955c635cbdd3267a3
凱寧 <21216740+kenny90263@users.noreply.github.com>
2020-09-09 16:47:56 +0800

    Create 99.php

---
+++ php/99.php
@@ -0,0 +1,16 @@
+<?php
+// 九九乘法表
+
+// 寫法 1
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        echo $i . "x" . $j . "=" . $i * $j . "&nbsp;&nbsp;&nbsp;&nbsp;";
+    }
+    echo "<br>";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
