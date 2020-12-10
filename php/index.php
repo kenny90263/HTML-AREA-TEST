fb35874bb10e42ab848d740ea4e62184f1c54c2a
凱寧 <21216740+kenny90263@users.noreply.github.com>
2020-12-10 09:39:50 +0800

    1210

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+
+phpinfo(8);
+
+
+
+
+
+
+
+//header("Content-type:application/pdf");
+// It will be called downloaded.pdf
+//header("Content-Disposition:attachment;filename=1090730.pdf");
+// The PDF source is in original.pdf
+//readfile("uploads/1090730.pdf");
