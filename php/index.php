4e8dd5ce68c2c05ec3a5fd37e7411d56693ddb45
凱寧 <21216740+kenny90263@users.noreply.github.com>
2021-01-26 09:44:13 +0800

    0126

--- php/index.php
+++ php/index.php
@@ -1,13 +1,46 @@
 <?php
 
 
+$str = array(
+	'<option value="3A01-A ">3A01-A </option>',
+	'<option value="3A01-B ">3A01-B </option>',
+	'<option value="3A02-A ">3A02-A </option>',
+	'<option value="3A02-B ">3A02-B </option>',
+	'<option value="3A03-A ">3A03-A </option>',
+	'<option value="3A03-B ">3A03-B </option>',
+	'<option value="3A05   ">3A05   </option>',
+	'<option value="3A06   ">3A06   </option>',
+	'<option value="3A07-A ">3A07-A </option>',
+	'<option value="3A07-B ">3A07-B </option>',
+	'<option value="3A08-A ">3A08-A </option>',
+	'<option value="3A08-B ">3A08-B </option>',
+	'<option value="3A09-A ">3A09-A </option>',
+	'<option value="3A09-B ">3A09-B </option>',
+	'<option value="3A09-C ">3A09-C </option>',
+	'<option value="3A09-D ">3A09-D </option>',
+	'<option value="3A10-A ">3A10-A </option>',
+	'<option value="3A10-B ">3A10-B </option>',
+	'<option value="3A10-C ">3A10-C </option>',
+	'<option value="3A10-D ">3A10-D </option>',
+	'<option value="3A11-A ">3A11-A </option>',
+	'<option value="3A11-B ">3A11-B </option>',
+	'<option value="3A12-A ">3A12-A </option>',
+	'<option value="3A12-B ">3A12-B </option>',
+	'<option value="3A13-A ">3A13-A </option>',
+	'<option value="3A13-B ">3A13-B </option>',
+	'<option value="3A15   ">3A15   </option>',
+	'<option value="3A16   ">3A16   </option>',
+	'<option value="3A17   ">3A17   </option>',
+	'<option value="3A18   ">3A18   </option>',
+	'<option value="3A19   ">3A19   </option>',
+	'<option value="3A20-A ">3A20-A </option>',
+	'<option value="3A20-B ">3A20-B </option>',
+	'<option value="3A20-C ">3A20-C </option>'
+);
 
-
-
-
-
-
-
+foreach($str as $strs){
+    echo strip_tags($strs)."<br>";
+}
 
 
 
