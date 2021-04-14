b59159a0a89195785af2b8bebff0511e56323db8
凱寧 <21216740+kenny90263@users.noreply.github.com>
2021-04-15 01:31:09 +0800

    20210415

--- php/index.php
+++ php/index.php
@@ -1,5 +1,9 @@
 <?php
-$url = 'http://books.google.com/books/content?id=ST-ZDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api';
-$image_data = file_get_contents($url);
-$image = imagecreatefromstring($image_data);
-imagejpeg($image, 'google-chart.jpg');
+
+
+phpinfo();
+
+// $url = 'http://books.google.com/books/content?id=ST-ZDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api';
+// $image_data = file_get_contents($url);
+// $image = imagecreatefromstring($image_data);
+// imagejpeg($image, 'google-chart.jpg');
